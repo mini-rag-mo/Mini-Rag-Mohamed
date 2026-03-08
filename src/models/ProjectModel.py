c0194e456ecbf4cdd5bf08bd1e0f7c53fea0719a
Mohamed <mohamedsibaq@outlook.com>
2026-03-08 09:09:22 +0200

    Enhance ProcessController and AssetModel; add error handling for file processing and asset retrieval

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -1,8 +1,7 @@
-from models.db_schemes import project
-
 from .BaseDataModel import BaseDataModel
 from .db_schemes import Project
 from .enums.DataBaseEnum import DataBaseEnum
+
 
 class ProjectModel(BaseDataModel):
 
