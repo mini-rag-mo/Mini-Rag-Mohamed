bdd834a720b7334ae8edc9818bd97f24436def6a
Mohamed <mohamedsibaq@outlook.com>
2026-05-02 17:27:19 +0300

    feat: Update entrypoint script to use environment variable for database URL in alembic.ini

--- docker/minirag/entrypoint.sh
+++ docker/minirag/entrypoint.sh
@@ -3,6 +3,10 @@ set -e
 
 echo "Running database migrations..."
 cd /app/models/db_schemes/minirag/
+
+# Replace the database URL in alembic.ini with the environment variable
+sed -i "s|sqlalchemy.url = .*|sqlalchemy.url = ${POSTGRES_URL}|" alembic.ini
+
 alembic upgrade head
 cd /app
 
