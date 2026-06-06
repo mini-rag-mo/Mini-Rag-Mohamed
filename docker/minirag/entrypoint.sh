1eae527067247bd66dc6cfb9ac3cf72b33fe36a7
Sibaq117 <Sibaq117@users.noreply.github.com>
2026-06-06 23:42:06 +0300

    Fix entrypoint.sh line endings for Linux

--- docker/minirag/entrypoint.sh
+++ docker/minirag/entrypoint.sh
@@ -1,22 +1,9 @@
-cat > docker/minirag/entrypoint.sh << 'EOF'
 #!/bin/bash
 set -e
 
 echo "Running database migrations..."
 cd /app/models/db_schemes/minirag/
-
-python3 -c "
-import re, os
-url = os.environ.get('POSTGRES_URL', '')
-with open('alembic.ini', 'r') as f:
-    content = f.read()
-content = re.sub(r'sqlalchemy\.url = .*', 'sqlalchemy.url = ' + url, content)
-with open('alembic.ini', 'w') as f:
-    f.write(content)
-"
-
 alembic upgrade head
 cd /app
 
-exec "\$@"
-EOF
+exec "$@"
